75c70d7e848212ae30dfd07f76eb9f9bf196ec63
ickah <barukab68@gmail.com>
2023-11-18 12:30:10 +0100

    add theme

---
+++ src/theme/ThemeToggle.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import React from "react";
+import { moon, sun } from "lucide-react";
+
+export const ThemeToggle = () => {
+  const { setTheme, theme } = useTheme();
+  return (
+    <div>
+      <Button
+        variant="ghost"
+        size="sm"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      ></Button>
+    </div>
+  );
+};
