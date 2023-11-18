1d819bfa7124ece171ca63cc8f57e03d4c315d13
ickah <barukab68@gmail.com>
2023-11-18 12:18:37 +0100

    add theme

--- features/layout/Header.tsx
+++ features/layout/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const Header = async () => {
   return (
     <header className="border-b border-b-accent">
-      <div className="container flex items-center py-2 max-w-lg m-auto gap-1">
+      <div className="container flex items-center py-2 max-w-lg m-auto gap-1 justify-between">
         <h2 className="text-2xl font-bold mr-auto">My thread</h2>
         <Button>demo</Button>
       </div>
