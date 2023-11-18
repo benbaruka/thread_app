c015950d3140a138c18266f52f509fd15a83c42d
ickah <barukab68@gmail.com>
2023-11-18 11:26:59 +0100

    add theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello world !</div>;
+  return (
+    <div>
+      Hello world !<Button>Hey click me</Button>
+      <Input />
+    </div>
+  );
 }
