6e2e4a0ea6d72da2f5f70f8898be3b979fddeab8
ickah <barukab68@gmail.com>
2023-11-18 13:00:12 +0100

    add theme

--- features/layout/Footer.tsx
+++ features/layout/Footer.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div className=""> 
-
-  </div>;
+  return <div className="py-2 flex justify-between container gap-1"></div>;
 };
