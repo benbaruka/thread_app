bfc1a4a36693f5cdc6b4ac4d4f9ee74bf82294cc
ickah <barukab68@gmail.com>
2023-11-18 13:27:39 +0100

    add theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/src/theme/ThemeProvider";
 import clsx from "clsx";
 import { Header } from "@/features/layout/Header";
+import { Footer } from "@/features/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,11 +22,12 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className={(clsx(inter.className), "bg-background h-full")}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex flex-col h-full">
+          <div className="flex-col h-full">
             <Header />
             <div className="flex-1 max-w-lg m-auto py-12 w-full">
               {children}
             </div>
+            <Footer />
           </div>
         </ThemeProvider>
       </body>
--- features/layout/Footer.tsx
+++ features/layout/Footer.tsx
@@ -1,7 +1,13 @@
+import { Home } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="py-2 flex justify-between container gap-1 fixed bottom-0 left-0 right-0 max-w-lg bg-background m-auto border-t border-accent"></div>
+    <div className="max-w-lg fixed bottom-0">
+      <Link href="/">
+        <Home />
+      </Link>
+    </div>
   );
 };
