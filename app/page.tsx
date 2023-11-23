22a1e862dca327b0034963e4f10597237496e319
ickah <barukab68@gmail.com>
2023-11-23 13:35:42 +0100

    add prisma and database

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { getAuthSession } from "@/lib/authOption";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
   return (
     <div>
+      <p>{JSON.stringify(0, null, 2)}</p>
       Hello world !<Button>Hey click me</Button>
       <Input />
     </div>
