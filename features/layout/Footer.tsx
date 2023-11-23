c27e8740586eb85500c8e6d7c5f8add72133d85e
ickah <barukab68@gmail.com>
2023-11-23 11:43:44 +0100

    add prisma and database

--- features/layout/Footer.tsx
+++ features/layout/Footer.tsx
@@ -7,24 +7,27 @@ import { buttonVariants } from "@/components/ui/button";
 export const Footer = () => {
   return (
     <div
-      className="py-2 flex justify-between container gap-1 bg-background max-w-lg m-auto border-t border-t-accent"
-      style={{ position: "fixed", bottom: "0", right: "0", left: "0" }}
+      className="py-2 flex justify-between container gap-1 bg-background max-w-lg m-auto border-t-accent"
+      style={{
+        position: "fixed",
+        bottom: "0",
+        right: "0",
+        left: "0",
+        borderTop: "1px solid hsl(var(--accent))",
+      }}
     >
-      <Link
-        href="/"
-        className={(clsx(buttonVariants({ variant: "ghost" })), "flex-1")}
-      >
+      <Link href="/" className={clsx(buttonVariants({ variant: "ghost" }))}>
         <Home size={20} />
       </Link>
       <Link
         href="/write"
-        className={(clsx(buttonVariants({ variant: "ghost" })), "flex-1")}
+        className={clsx(buttonVariants({ variant: "ghost" }))}
       >
         <PenSquare size={20} />
       </Link>
       <Link
         href="/profile"
-        className={(clsx(buttonVariants({ variant: "ghost" })), "flex-1")}
+        className={clsx(buttonVariants({ variant: "ghost" }))}
       >
         <User size={20} />
       </Link>
