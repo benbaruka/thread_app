5b9a923565444581e6ebefb2a090ffcb5b9346e1
ickah <barukab68@gmail.com>
2023-11-23 11:25:49 +0100

    design down

--- features/layout/Footer.tsx
+++ features/layout/Footer.tsx
@@ -1,17 +1,31 @@
 import { Home } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import clsx from "clsx";
+import { buttonVariants } from "@/components/ui/button";
 
 export const Footer = () => {
   return (
-    <div style={{ position: "fixed" }}>
-      <Link href="/">
+    <div
+      className="py-2 flex justify-between container gap-1 bg-background max-w-lg m-auto border-t border-t-accent"
+      style={{ position: "fixed", bottom: "0", right: "0", left: "0" }}
+    >
+      <Link
+        href="/"
+        className={(clsx(buttonVariants({ variant: "ghost" })), "flex-1")}
+      >
         <Home size={20} />
       </Link>
-      <Link href="/">
+      <Link
+        href="/"
+        className={(clsx(buttonVariants({ variant: "ghost" })), "flex-1")}
+      >
         <Home size={20} />
       </Link>
-      <Link href="/">
+      <Link
+        href="/"
+        className={(clsx(buttonVariants({ variant: "ghost" })), "flex-1")}
+      >
         <Home size={20} />
       </Link>
     </div>
